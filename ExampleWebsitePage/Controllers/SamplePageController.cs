2f48b191b0a88a58fff51ec5c9ea48004cbdf358
Jared McGuire <jared.mcguire@gmail.com>
2011-05-21 21:27:08 -0500

    Add ExampleWebsitePage project.

---
+++ ExampleWebsitePage/Controllers/SamplePageController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace ExampleWebsitePage.Controllers
+{
+	public class SamplePageController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View("Index");
+		}
+	}
+}
---
+++ ExampleWebsitePage/Routing/RouteRegistrator.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+using MvcTurbine.Routing;
+
+namespace ExampleWebsitePage.Routing
+{
+	public class RouteRegistrator : IRouteRegistrator
+	{
+		public void Register(RouteCollection routes)
+		{
+			routes.MapRoute("SamplePage", "SamplePage", new { controller = "SamplePage", action = "Index" });
+		}
+	}
+}
